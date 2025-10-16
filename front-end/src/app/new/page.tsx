99684c57b5ececf32b48fec8226349d75529aaaa
Mohamed-elhediKraiem <113349583+Mohamed-elhediKraiem@users.noreply.github.com>
2025-10-16 16:54:57 +0200

    feat : ajout backend by env file

--- front-end/src/app/new/page.tsx
+++ front-end/src/app/new/page.tsx
@@ -34,8 +34,9 @@ export default function NewCandidaturePage() {
 
     try {
       // ✅ Appel à ton backend Python
+      const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
       const res = await fetch(
-        `http://localhost:8000/scrape?url=${encodeURIComponent(url)}`
+        `${baseUrl}/scrape?url=${encodeURIComponent(url)}`
       );
       if (!res.ok) throw new Error("Erreur du serveur scraping");
 
